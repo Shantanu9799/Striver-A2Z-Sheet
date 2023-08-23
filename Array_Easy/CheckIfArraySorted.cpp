c0c6ec4f6091f86a05c1a0d1725cab6121c85daa
Shantanu9799 <cshantanu712@gmail.com>
2023-08-23 20:17:07 +0530

    easy 3

---
+++ Array_Easy/CheckIfArraySorted.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(vector<int>& a) {
+        int n = a.size();
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+        if(a[i] > a[(i+1)%n]) {
+            cnt++;
+        }
+    }
+    return cnt<=1;
+    }
+};
