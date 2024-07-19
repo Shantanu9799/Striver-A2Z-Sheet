e07ff9d6d4fbf721cf808e010e70d045dc50c354
Shantanu Chatterjee <85756136+Shantanu9799@users.noreply.github.com>
2024-07-19 11:28:21 +0530

    Binary Serach

---
+++ Binary_Search/SearchElementINSortedRotatedArray.cpp
@@ -0,0 +1,33 @@
+int search(vector<int>& nums, int n, int target)
+{
+    // Write your code here.
+    // Return the position of K in ARR else return -1.
+    int left = 0, right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // Check if the left part is sorted
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        // Right part must be sorted
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
