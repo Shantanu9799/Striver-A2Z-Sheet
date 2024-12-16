e99c8ab5e7e784eebf81d5d15098ee60cd9c99ed
Shantanu Chatterjee <85756136+Shantanu9799@users.noreply.github.com>
2024-12-16 13:03:17 +0530

    Update SumOfNelements.cpp

--- Know_Basic_Recusion/SumOfNelements.cpp
+++ Know_Basic_Recusion/SumOfNelements.cpp
@@ -1,4 +1,25 @@
 class Solution {
+  public:
+  
+    // helper function to calculate the sum
+    int helperFunction(int n, int sum) {
+        if(n == 0) {
+            return sum;
+        }
+        int toBeAdded = n * n * n;
+        sum += toBeAdded;
+        return helperFunction(n-1, sum);
+    }
+  
+    int sumOfSeries(int n) {
+        // code here
+        return helperFunction(n, 0);
+    }
+};
+
+
+
+class Solution {
   long long sum = 0;
   public:
     long long sumOfSeries(long long n) {
